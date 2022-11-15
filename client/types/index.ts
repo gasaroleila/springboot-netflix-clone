fc381c7899e8dab602e50050609ef5cffce20ee7
gasaroleila <uwamgaleila@gmail.com>
2022-11-15 16:43:30 +0200

    added client side

---
+++ client/types/index.ts
@@ -0,0 +1,56 @@
+import { Breakpoint } from '../config/breakpoints';
+
+export type Maybe<T> = T | null;
+
+export type Dimension = {
+  height: number;
+  width: number;
+};
+
+export type DimensionDetail = {
+  dimension: Dimension;
+  breakpoint: Breakpoint;
+  isMobile: boolean;
+  isTablet: boolean;
+  isDesktop: boolean;
+};
+
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+export enum MediaType {
+  MOVIE = 'movie',
+  TV = 'tv'
+}
+
+export type Media = {
+  id: number;
+  title: string;
+  overview: string;
+  poster: string;
+  banner: string;
+  rating: number;
+  genre: Genre[];
+};
+
+export type ImageType = 'poster' | 'original';
+
+export type Section = {
+  heading: string;
+  endpoint: string;
+  defaultCard?: boolean;
+  topList?: boolean;
+};
+
+export type LoginInputs = {
+  email: string
+  password: string
+}
+
+export type SignUpInputs = {
+  username: string
+  email: string
+  password: string
+}
